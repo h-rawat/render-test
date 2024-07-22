485bb03349df38a0066e63a8f152f301af0d41ab
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-07-23 01:09:31 +0530

    setup project structure

--- models/note.js
+++ models/note.js
@@ -1,30 +1,20 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to url");
-
-mongoose
-  .connect(url)
-  .then((result) => console.log("connected to mongodb"))
-  .catch((error) => console.log("error connecting to mongodb", error.message));
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5
   },
   important: Boolean,
-});
+})
 
-noteSchema.set("toJSON", {
+noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject.__v;
-    delete returnedObject._id;
-  },
-});
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
