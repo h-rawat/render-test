ea43e8416382338fa091a18baf561592333e7f03
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2024-07-24 01:40:40 +0530

    tests

--- controllers/notes.js
+++ controllers/notes.js
@@ -8,17 +8,14 @@ notesRouter.get('', async (req, res) => {
 })
 
 // get a particular note
-notesRouter.get('/:id', (req, res, next) => {
-  Note.findById(req.params.id)
-    .then((note) => {
-      if (note) res.json(note)
-      else res.status(404).end()
-    })
-    .catch((error) => next(error))
+notesRouter.get('/:id', async (req, res) => {
+  const note = Note.findById(req.params.id)
+  if (note) res.json(note)
+  else res.status(404).end()
 })
 
 // create a note
-notesRouter.post('/', (req, res, next) => {
+notesRouter.post('/', async (req, res) => {
   const body = req.body
 
   if (body.content === undefined) {
@@ -32,19 +29,14 @@ notesRouter.post('/', (req, res, next) => {
     important: body.important || false,
   })
 
-  note
-    .save()
-    .then((savedNote) => {
-      res.json(savedNote)
-    })
-    .catch((error) => next(error))
+  const savedNote = await note.save()
+  res.status(201).json(savedNote)
 })
 
 // delete a particular note
-notesRouter.delete('/:id', (req, res, next) => {
-  Note.findByIdAndDelete(req.params.id)
-    .then(() => res.status(204).end())
-    .catch((error) => next(error))
+notesRouter.delete('/:id', async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id)
+  res.status(204).end()
 })
 
 // update a note
